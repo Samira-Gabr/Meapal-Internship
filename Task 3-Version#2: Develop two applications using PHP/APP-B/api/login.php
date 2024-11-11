5fd7e6248dd45bb5afeab73c37e85441aeb0e692
Samira Ashraf <156571600+Samira-Gabr@users.noreply.github.com>
2024-11-11 23:36:15 +0200

    Add files via upload

---
+++ Task 3-Version#2: Develop two applications using PHP/APP-B/api/login.php
@@ -0,0 +1,53 @@
+<?php
+require 'dbconn.php';
+header('Content-Type: application/xml');
+define('API_TOKEN', 'Token 2#7#7#2#0#0#0');
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $header = getallheaders();
+    if (!isset($header['Authorization']) || $header['Authorization'] !== API_TOKEN) {
+    /*------------------------------------*/
+     $response = new SimpleXMLElement('<response/>');
+     $response->addChild('status', 'error');
+     $response->addChild('message', 'Invalid API_token :(');
+     echo $response->asXML();
+     exit();
+    }
+    $xmlRecived = file_get_contents('php://input');
+    $xml = simplexml_load_string($xmlRecived);
+    $login = (string)$xml->login;
+    $password =(string)$xml->password;
+    /*---------------------------------*/
+    if (filter_var($login, FILTER_VALIDATE_EMAIL)) {
+        $SQL_Q = "SELECT * FROM users WHERE email = :login";
+    } else {
+        $SQL_Q = "SELECT * FROM users WHERE username = :login";
+    }
+    $stmt = $pdo->prepare($SQL_Q);
+    $stmt->execute(['login' => $login]);
+    $user = $stmt->fetch();
+    /*---------------------------------------------- */
+    $response = new SimpleXMLElement('<response/>');
+    if ($user && password_verify($password, $user['password'])) {
+        //$response = new SimpleXMLElement('<response/>');
+        $response->addChild('status','success');
+        $response->addChild('user_id', $user['id']);
+        $response->addChild('username', $user['username']);
+        $response->addChild('role', $user['role']);
+        //echo $response->asXML();
+    } else {
+        //$response = new SimpleXMLElement('</response>');
+        $response->addChild('status', 'error');
+        $response->addChild('message', 'Invalid credentials.');
+        //echo $response->asXML();
+    }
+    echo $response->asXML();
+
+} else {
+    $response = new SimpleXMLElement('<response/>');
+    $response->addChild('status', 'error');
+    $response->addChild('message', 'Invalid request method');
+    echo $response->asXML();
+}
+
+?>
---
+++ Task 3-Version#2: Develop two applications using PHP/APP-B/api/view_posts.php
@@ -0,0 +1,46 @@
+<?php
+require 'dbconn.php';
+header('Content-Type: application/xml');
+define('API_TOKEN', 'Token 2#7#7#2#0#0#0');
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $header = getallheaders();
+    if (!isset($header['Authorization']) || $header['Authorization'] !== API_TOKEN) {
+      
+      $response = new SimpleXMLElement('<response/>');
+      $response->addChild('status', 'error');
+      $response->addChild('message', 'Incorrect API token');
+      echo $response->asXML();
+      exit();
+    }
+    $response = new SimpleXMLElement('<response/>');
+    
+    $SQL_Q = "SELECT posts.*, users.username FROM posts INNER JOIN users ON posts.user_id = users.id WHERE posts.approved = 1";
+    $stmt  = $pdo->prepare($SQL_Q);
+    $stmt->execute();
+    $posts = $stmt->fetchAll();
+    
+    if ($posts) {
+        $response->addChild('status', 'success');
+        foreach ($posts as $post) {
+            $postxml = $response->addChild('post');
+            $postxml->addChild('title', htmlspecialchars($post['title']));
+            $postxml->addChild('username', htmlspecialchars($post['username']));
+            $postxml->addChild('content', htmlspecialchars($post['content']));
+            if ($post['image']) {
+                $postxml->addChild('image', htmlspecialchars($post['image']));
+            }
+        }
+    } else {
+        $response->addChild('status', 'error');
+        $response->addChild('message', 'No posts available.');
+    }
+    echo $response->asXML();
+
+} else {
+    $response = new SimpleXMLElement('<response/>');
+    $response->addChild('status', 'error');
+    $response->addChild('message', 'Invalid request method');
+    echo $response->asXML();
+}
+?>
